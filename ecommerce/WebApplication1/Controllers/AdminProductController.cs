3b4c053d3865028b15e52173d6fe86f5b8353fdc
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-10-11 15:12:00 +0300

    no comment

--- ecommerce/WebApplication1/Controllers/AdminProductController.cs
+++ ecommerce/WebApplication1/Controllers/AdminProductController.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
+using PagedList.Mvc;
+using PagedList;
 
 namespace WebApplication1.Controllers
 {
@@ -15,9 +17,9 @@ namespace WebApplication1.Controllers
     {
         ProductRepository productRepository = new ProductRepository();
         DataContext db = new DataContext();
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View(productRepository.List());
+            return View(productRepository.List().ToPagedList(page, 3));
         }
 
         [HttpGet]
